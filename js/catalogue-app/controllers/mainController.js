b2c434089b1655491c83139e52d1f7f90d29b568
MarcMagnin <marc.magnin@gmail.com>
2015-01-07 00:51:53 +0100

    up

--- js/catalogue-app/controllers/mainController.js
+++ js/catalogue-app/controllers/mainController.js
@@ -33,7 +33,7 @@ app.controller("mainController", ['$scope', '$rootScope', '$http', '$timeout', '
     $scope.searchWord = "";
     //$rootScope.apiRootUrl = "http://82.226.165.21:8081/databases/BookFlo";
     //$rootScope.apiRootUrl = "http://localhost:8081/databases/BookFlo";
-    $rootScope.apiRootUrl = "http://localhost:8085/databases/BookFlo";
+    $rootScope.apiRootUrl = "http://62.23.104.30:3000/databases/BookFlo";
     $scope.items = [];
     $scope.tags = [];
     
@@ -209,7 +209,7 @@ app.controller("mainController", ['$scope', '$rootScope', '$http', '$timeout', '
  
 
     $scope.deleteItem = function ($index, item) {
-        
+        if( item.Images &&  item.Images.length>0)
         $http({
             method: 'DELETE',
             url: $rootScope.apiRootUrl +'/'+ item.Images[0].Url
